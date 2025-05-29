a2ae3cd21ba0b6dea222ceb9614ffd0b0b47a5a4
DEPELI ALTAN <altan.depeli@etu.unistra.fr>
2025-05-29 11:12:38 +0200

    ton message ici

--- app/page.js
+++ app/page.js
@@ -1,13 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
+import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-import Blog from "./components/homepage/blog"; // ← conserve comme ça
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+        const data = await res.json();
+        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+        setBlogs(filtered);
+      } catch (err) {
+        console.error("Erreur blog:", err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div suppressHydrationWarning>
       <HeroSection />
@@ -16,7 +37,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
-      <Blog /> {/* → Devient client-side */}
+      <Blog blogs={blogs} />
       <ContactSection />
     </div>
   );
