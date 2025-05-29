f0201a375b9e8234265424f8ee4ba7af32db9933
DEPELI ALTAN <altan.depeli@etu.unistra.fr>
2025-05-29 12:10:14 +0200

    ton message ici

--- app/page.js
+++ app/page.js
@@ -1,19 +1,26 @@
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
+import dynamic from "next/dynamic";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+const AboutSection = dynamic(() => import("./components/homepage/about"), {
+  ssr: false,
+});
+
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
-      {/* <Experience />
-      <Skills />
+      <Experience />
+      {/* <Skills />
       <Projects />
       <Education />
       <ContactSection /> */}
